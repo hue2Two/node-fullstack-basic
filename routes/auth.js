2e9f556888a2085c11cafa56b2d080fefbc0e899
hue2Two <h.faisal3399@gmail.com>
2024-02-01 18:55:27 -0500

    implementing

--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,79 @@ router.get('/logout', authController.logout, (req, res) => {
     console.log(`req: ${req.url} , method: ${req.method}`);
 });
 
+router.post('/logout', async (req, res) => {
+    console.log(`playtime data sent: ${JSON.stringify(req.body)}`);
+    // let retrievedData = JSON.stringify(req.body.sendPlayTime);
+    let retrievedData = req.body.sendPlayTime;
+
+    let thing1toSend;
+    let thing2toSend;
+    let thing3toSend;
+
+    //filtering cookie
+        // Extract the JWT token from the cookie header
+        const cookies = req.headers.cookie.split('; ');
+        const jwtCookie = cookies.find(cookie => cookie.startsWith('jwt='));
+        const token = jwtCookie ? jwtCookie.split('=')[1] : null;
+        console.log(`JWT INFO TIME SEND P1: ${cookies}`);
+        console.log(`JWT INFO TIME SEND P2: ${jwtCookie}`);
+        console.log(`JWT INFO TIME SEND P3: ${token}`);
+
+        //decode token in cookie & grab id
+        const decoded = await promisify(jwt.verify)(token, jwtSecret);
+        console.log(`JWT INFO TIME SEND P4: ${JSON.stringify(decoded)}`);
+
+    retrievedData.forEach(thing => {
+        console.log(`thing: ${thing}`);
+
+        if(thing.includes("playTime")) {
+            // thing1toSend = thing;
+            let temp = thing.split("=");
+            thing1toSend = temp[1];
+            thing3toSend = temp[0];
+        } else {
+            thing2toSend = thing;
+        }
+    })
+
+    console.log(`thing1: ${thing1toSend}, thing2: ${thing2toSend}`);
+    
+    //check if token matches id in db
+    db.query(`SELECT email FROM test WHERE id = ?`, [decoded.tokenId], (error, result) => {
+        console.log(`CHECKING JWT RESULT SENDING TIME 1: ${JSON.stringify(result)}`);
+
+        if (result.length > 0) {
+            const userEmail = result[0].email;
+
+            //insert info into the db
+            db.query(`INSERT INTO gametimerecord (
+                email,
+                gameTime
+            )
+            
+            VALUES (
+                '${userEmail}',
+                '${thing1toSend}'
+            )`);
+
+            // res.json(gameToAdd);
+            // res.cookie('playTime', 'logout');
+            // res.cookie('playTime', '', { expires: new Date(0), path: '/' });
+
+            // res.json({
+            //     cookieName: thing3toSend
+            // })
+
+        } else {
+            return res.status(404).json({ message: 'User not found' });
+        }
+    })
+
+    res.json({
+        cookieName: thing3toSend
+    })
+});
+
 // router.post('/logout', (req, res) => {
 //     console.log(`playtime data sent: ${req.body}`);
 // });
--- routes/pages.js
+++ routes/pages.js
@@ -32,6 +32,17 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/contact', (req, res) => {
+    console.log(`req: ${req.url} , method: ${req.method}`);
+
+    //render home route
+    // console.log(`TESTING ISLOGGEDIN 4: ${isLoggedInHelper}`);
+    // res.render('home');
+    res.render('contact', {
+        isLoggedInHelper: isLoggedInHelper
+    })
+})
+
 // router.post('/', (req, res) => {
 //     console.log(`req: ${req.url} , method: ${req.method}`);
 //     console.log(`game data from frontend: ${JSON.stringify(req.body)}`);
@@ -205,7 +216,10 @@ router.get('/register', (req, res) => {
     console.log(`req: ${req.url} , method: ${req.method}`);
 
     //render home route
-    res.render('register');
+    // res.render('register');
+    res.render('register', {
+        isLoggedInHelper: isLoggedInHelper
+    })
 })
 
 router.get('/login', (req, res) => {
@@ -320,78 +334,78 @@ router.get('/profile', authController.isLoggedIn, (req, res) => {
 
 console.log(`isloggedinvalue 3: ${isLoggedInHelper}`);
 
-router.post('/logout', async (req, res) => {
-    console.log(`playtime data sent: ${JSON.stringify(req.body)}`);
-    // let retrievedData = JSON.stringify(req.body.sendPlayTime);
-    let retrievedData = req.body.sendPlayTime;
+// router.post('/logout', async (req, res) => {
+//     console.log(`playtime data sent: ${JSON.stringify(req.body)}`);
+//     // let retrievedData = JSON.stringify(req.body.sendPlayTime);
+//     let retrievedData = req.body.sendPlayTime;
 
-    let thing1toSend;
-    let thing2toSend;
-    let thing3toSend;
+//     let thing1toSend;
+//     let thing2toSend;
+//     let thing3toSend;
 
-    //filtering cookie
-        // Extract the JWT token from the cookie header
-        const cookies = req.headers.cookie.split('; ');
-        const jwtCookie = cookies.find(cookie => cookie.startsWith('jwt='));
-        const token = jwtCookie ? jwtCookie.split('=')[1] : null;
-        console.log(`JWT INFO TIME SEND P1: ${cookies}`);
-        console.log(`JWT INFO TIME SEND P2: ${jwtCookie}`);
-        console.log(`JWT INFO TIME SEND P3: ${token}`);
+//     //filtering cookie
+//         // Extract the JWT token from the cookie header
+//         const cookies = req.headers.cookie.split('; ');
+//         const jwtCookie = cookies.find(cookie => cookie.startsWith('jwt='));
+//         const token = jwtCookie ? jwtCookie.split('=')[1] : null;
+//         console.log(`JWT INFO TIME SEND P1: ${cookies}`);
+//         console.log(`JWT INFO TIME SEND P2: ${jwtCookie}`);
+//         console.log(`JWT INFO TIME SEND P3: ${token}`);
 
-        //decode token in cookie & grab id
-        const decoded = await promisify(jwt.verify)(token, jwtSecret);
-        console.log(`JWT INFO TIME SEND P4: ${JSON.stringify(decoded)}`);
+//         //decode token in cookie & grab id
+//         const decoded = await promisify(jwt.verify)(token, jwtSecret);
+//         console.log(`JWT INFO TIME SEND P4: ${JSON.stringify(decoded)}`);
 
-    retrievedData.forEach(thing => {
-        console.log(`thing: ${thing}`);
+//     retrievedData.forEach(thing => {
+//         console.log(`thing: ${thing}`);
 
-        if(thing.includes("playTime")) {
-            // thing1toSend = thing;
-            let temp = thing.split("=");
-            thing1toSend = temp[1];
-            thing3toSend = temp[0];
-        } else {
-            thing2toSend = thing;
-        }
-    })
+//         if(thing.includes("playTime")) {
+//             // thing1toSend = thing;
+//             let temp = thing.split("=");
+//             thing1toSend = temp[1];
+//             thing3toSend = temp[0];
+//         } else {
+//             thing2toSend = thing;
+//         }
+//     })
 
-    console.log(`thing1: ${thing1toSend}, thing2: ${thing2toSend}`);
+//     console.log(`thing1: ${thing1toSend}, thing2: ${thing2toSend}`);
     
-    //check if token matches id in db
-    db.query(`SELECT email FROM test WHERE id = ?`, [decoded.tokenId], (error, result) => {
-        console.log(`CHECKING JWT RESULT SENDING TIME 1: ${JSON.stringify(result)}`);
+//     //check if token matches id in db
+//     db.query(`SELECT email FROM test WHERE id = ?`, [decoded.tokenId], (error, result) => {
+//         console.log(`CHECKING JWT RESULT SENDING TIME 1: ${JSON.stringify(result)}`);
 
-        if (result.length > 0) {
-            const userEmail = result[0].email;
+//         if (result.length > 0) {
+//             const userEmail = result[0].email;
 
-            //insert info into the db
-            db.query(`INSERT INTO gametimerecord (
-                email,
-                gameTime
-            )
+//             //insert info into the db
+//             db.query(`INSERT INTO gametimerecord (
+//                 email,
+//                 gameTime
+//             )
             
-            VALUES (
-                '${userEmail}',
-                '${thing1toSend}'
-            )`);
+//             VALUES (
+//                 '${userEmail}',
+//                 '${thing1toSend}'
+//             )`);
 
-            // res.json(gameToAdd);
-            // res.cookie('playTime', 'logout');
-            // res.cookie('playTime', '', { expires: new Date(0), path: '/' });
+//             // res.json(gameToAdd);
+//             // res.cookie('playTime', 'logout');
+//             // res.cookie('playTime', '', { expires: new Date(0), path: '/' });
 
-            // res.json({
-            //     cookieName: thing3toSend
-            // })
+//             // res.json({
+//             //     cookieName: thing3toSend
+//             // })
 
-        } else {
-            return res.status(404).json({ message: 'User not found' });
-        }
-    })
+//         } else {
+//             return res.status(404).json({ message: 'User not found' });
+//         }
+//     })
 
-    res.json({
-        cookieName: thing3toSend
-    })
-});
+//     res.json({
+//         cookieName: thing3toSend
+//     })
+// });
 
 router.get('/gameTimeRenew', async (req, res) => {
     try {
